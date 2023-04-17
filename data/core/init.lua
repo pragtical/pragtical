9ec22ab7cf00ce797ccfe343111ae61a45682c26
jgmdev <jgmdev@gmail.com>
2023-04-17 18:24:16 -0400

    Reduce default coroutine timeout from lite-xl/lite-xl#1468

--- data/core/init.lua
+++ data/core/init.lua
@@ -1421,7 +1421,7 @@ local run_threads = coroutine.wrap(function()
             core.threads[k] = nil
           end
         else
-          wait = wait or (1/30)
+          wait = wait or 0.001
           thread.wake = system.get_time() + wait
           minimal_time_to_wake = math.min(minimal_time_to_wake, wait)
         end
