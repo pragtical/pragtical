5e531aa25391621af84cf82ea5a9bc5d440d7c30
Adam Harrison <adamdharrison@gmail.com>
2023-12-03 11:09:56 -0500

    Fixed a minor bug, should close issue #1680.

--- data/plugins/drawwhitespace.lua
+++ data/plugins/drawwhitespace.lua
@@ -360,7 +360,7 @@ end
 
 command.add(nil, {
   ["draw-whitespace:toggle"]  = function()
-    config.plugins.drawwhitespace.enabled = not config.drawwhitespace.enabled
+    config.plugins.drawwhitespace.enabled = not config.plugins.drawwhitespace.enabled
   end,
 
   ["draw-whitespace:disable"] = function()
