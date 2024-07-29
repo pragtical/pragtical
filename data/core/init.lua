pragtical/pragtical

commit de4c28988aede8ed220b447fe5bb296037f24c10

Auto chdir to project depending on active document (#93)
This change better supports plugins like the terminal
without them having to implement current project path detection.
When editing files not part of the root project the system will
automatically chdir to their current parent directory.

Other changes include:

* added system.getcwd():string
* added core.current_project(filename?:string):core.project
* added Project:path_belongs_to(path):boolean
* increased the mod minor version as a result of newly introduced
  functions

data/core/init.lua
@@ -93,6 +93,13 @@ function core.remove_project(project, force)
     if project == core.projects[i] or project == core.projects[i].path then
       local project = core.projects[i]
       table.remove(core.projects, i)
+      if
+        core.projects[1]
+        and
+        common.normalize_volume(system.getcwd()) == project.path
+      then
+        system.chdir(core.projects[1].path)
+      end
       return project
     end
   end
@@ -102,7 +109,9 @@ end
 
 function core.set_project(project)
   while #core.projects > 0 do core.remove_project(core.projects[#core.projects], true) end
-  return core.add_project(project)
+  local project_object = core.add_project(project)
+  system.chdir(project_object.path)
+  return project_object
 end
 
 
@@ -111,6 +120,39 @@ function core.open_project(project)
   core.root_view:close_all_docviews()
   reload_customizations()
   update_recents_project("add", project.path)
+end
+
+
+---Get project for currently opened DocView or given filename path.
+---If the given path does not belongs to any of the opened projects a new
+---project object will be created and returned using the directory of the
+---given filename path.
+---@param filename? string
+---@return core.project?
+function core.current_project(filename)
+  if not filename then
+    if
+      core.active_view:extends(DocView)
+      and
+      core.active_view.doc and core.active_view.doc.abs_filename
+    then
+      filename = core.active_view.doc.abs_filename
+    else
+      return core.projects[1]
+    end
+  end
+  if #core.projects > 1 then
+    for _, project in ipairs(core.projects) do
+      if project:path_belongs_to(filename) then
+        return project
+      end
+    end
+  end
+  if core.projects[1] and core.projects[1]:path_belongs_to(filename) then
+    return core.projects[1]
+  end
+  local dirname = common.dirname(filename)
+  if dirname then return Project(dirname) end
 end
 
 
@@ -957,6 +999,16 @@ function core.set_active_view(view)
     end
     core.last_active_view = core.active_view
     core.active_view = view
+  end
+  if
+    core.active_view:extends(DocView)
+    and
+    core.active_view.doc and core.active_view.doc.abs_filename
+  then
+    local project = core.current_project(
+      core.active_view.doc.abs_filename
+    )
+    if project then system.chdir(project.path) end
   end
 end
 

data/core/project.lua
@@ -74,6 +74,24 @@ function Project:normalize_path(filename)
 end
 
 
+---Checks if the given path belongs to the project.
+---@param path string
+---@return boolean
+function Project:path_belongs_to(path)
+  if not common.is_absolute_path(path) then
+    path = common.normalize_path(self.path .. PATHSEP .. path)
+    if not path or not system.get_file_info(path) then
+      return false
+    end
+    return true
+  end
+  if common.path_belongs_to(path, self.path) then
+    return true
+  end
+  return false
+end
+
+
 local function fileinfo_pass_filter(info, ignore_compiled)
   if info.size >= config.file_size_limit * 1e6 then return false end
   local basename = common.basename(info.filename)

data/core/start.lua
@@ -1,7 +1,7 @@
 -- this file is used by pragtical to setup the Lua environment when starting
 VERSION = "@PROJECT_VERSION@"
 MOD_VERSION_MAJOR = 3
-MOD_VERSION_MINOR = 4
+MOD_VERSION_MINOR = 5
 MOD_VERSION_PATCH = 0
 MOD_VERSION_STRING = string.format("%d.%d.%d", MOD_VERSION_MAJOR, MOD_VERSION_MINOR, MOD_VERSION_PATCH)
 

docs/api/system.lua
@@ -194,6 +194,12 @@ function system.rmdir(path) end
 function system.chdir(path) end
 
 ---
+---Get the current working directory.
+---
+---@return string directory Current working directgory.
+function system.getcwd() end
+
+---
 ---Truncates a file to a set length.
 ---
 ---@param file file* A file handle returned by io.open().

src/api/system.c
@@ -797,6 +797,24 @@ static int f_chdir(lua_State *L) {
 }
 
 
+static int f_getcwd(lua_State* L) {
+  #ifdef _WIN32
+    wchar_t buffer[MAX_PATH];
+    if (!_wgetcwd(buffer, sizeof(buffer)))
+      return luaL_error(L, "error getcwd: %s", strerror(errno));
+    char *utf8_buffer = utfconv_wctoutf8(buffer);
+    lua_pushstring(L, utf8_buffer);
+    free(utf8_buffer);
+  #else
+    char buffer[PATH_MAX];
+    if (!getcwd(buffer, sizeof(buffer)))
+      return luaL_error(L, "error getcwd: %s", strerror(errno));
+    lua_pushstring(L, buffer);
+  #endif
+  return 1;
+}
+
+
 static int f_list_dir(lua_State *L) {
   const char *path = luaL_checkstring(L, 1);
 
@@ -1423,6 +1441,7 @@ static const luaL_Reg lib[] = {
   { "show_fatal_error",    f_show_fatal_error    },
   { "rmdir",               f_rmdir               },
   { "chdir",               f_chdir               },
+  { "getcwd",              f_getcwd              },
   { "ftruncate",           f_ftruncate           },
   { "mkdir",               f_mkdir               },
   { "list_dir",            f_list_dir            },

commit 922dbf36ab4bf3055221eea48f8b1c51c83fbf2e

Fix for IPC plugin to open relative path
This fix restores opening of a relative path from within the IPC plugin
which was broken by #93

data/core/init.lua
@@ -444,6 +444,7 @@ function core.init()
   core.visited_files = {}
   core.restart_request = false
   core.quit_request = false
+  core.init_working_dir = system.getcwd()
 
   -- We load core views before plugins that may need them.
   ---@type core.rootview

data/plugins/ipc.lua
@@ -871,6 +871,7 @@ system.get_time = function()
     if primary_instance and ARGS[2] then
       local open_directory = false
       for i=2, #ARGS do
+        system.chdir(core.init_working_dir)
         local path = system.absolute_path(ARGS[i])
 
         if path then