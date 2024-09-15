72fa223c258f199dc3c5b2dc86d124fd8c844ea1
jgmdev <jgmdev@gmail.com>
2024-09-15 14:52:46 -0400

    Use project version as mods version
    Since we are following a semver approach to versioning it is much
    easier to just use a single version number that represents both, the
    project version and the mods version.
    
    Taken from https://semver.org/ and for reference:
    
    1. MAJOR version when you make incompatible API changes
    2. MINOR version when you add functionality in a backward compatible manner
    3. PATCH version when you make backward compatible bug fixes

--- data/core/start.lua
+++ data/core/start.lua
@@ -1,8 +1,8 @@
 -- this file is used by pragtical to setup the Lua environment when starting
 VERSION = "@PROJECT_VERSION@"
-MOD_VERSION_MAJOR = 3
-MOD_VERSION_MINOR = 5
-MOD_VERSION_PATCH = 0
+MOD_VERSION_MAJOR = tonumber("@MOD_VERSION_MAJOR@")
+MOD_VERSION_MINOR = tonumber("@MOD_VERSION_MINOR@")
+MOD_VERSION_PATCH = tonumber("@MOD_VERSION_PATCH@")
 MOD_VERSION_STRING = string.format("%d.%d.%d", MOD_VERSION_MAJOR, MOD_VERSION_MINOR, MOD_VERSION_PATCH)
 
 DEFAULT_SCALE = 1.0
