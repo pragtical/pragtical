ce0ed4192d217bcaa8c5244bba258a521e16daab
jgmdev <jgmdev@gmail.com>
2025-05-30 13:36:01 -0400

    CI: Polyfill GLIBC for backward compatibility
    We now use https://github.com/corsix/polyfill-glibc in order to
    patch the pragtical binary to be backward compatible with older
    glibc versions.
    
    Other Changes:
    
    * Updated to latest AppImage runtime with static libfuse.
    * Switched to new ubuntu aarch64 runners.

--- scripts/common.sh
+++ scripts/common.sh
@@ -76,6 +76,43 @@ get_default_build_dir() {
   echo "build-$platform-$arch"
 }
 
+polyfill_glibc() {
+  local platform
+  platform=$(get_platform_name)
+
+  if [[ "$platform" != "linux" ]]; then
+    return 0
+  fi
+
+  local arch
+  arch=$(get_platform_arch)
+
+  if [ ! -e "polyfill-glibc" ]; then
+    if ! wget -O "polyfill-glibc" "https://github.com/pragtical/polyfill-glibc/releases/download/binaries/polyfill-glibc.${arch}" ; then
+      echo "Could not download polyfill-glibc for the arch '${arch}'."
+      exit 1
+    else
+      chmod 0755 "polyfill-glibc"
+    fi
+  fi
+
+  local rename_symbols=""
+  if [[ "$arch" == "aarch64" ]]; then
+    local symbols="aarch_symbols_rename.txt"
+    echo "__isoc23_strtol@GLIBC_2.38 strtol" > $symbols
+    echo "__isoc23_strtoll@GLIBC_2.38 strtoll" >> $symbols
+    echo "__isoc23_strtoul@GLIBC_2.38 strtoul" >> $symbols
+    echo "__isoc23_strtoull@GLIBC_2.38 strtoull" >> $symbols
+    rename_symbols="--rename-dynamic-symbols=${symbols}"
+  fi
+
+  local binary_path="$1"
+  echo "======================================================================="
+  echo "Polyfill GLIBC on: ${binary_path}"
+  echo "======================================================================="
+  ./polyfill-glibc --target-glibc=2.17 $rename_symbols "$binary_path"
+}
+
 if [[ $(get_platform_name) == "UNSUPPORTED-OS" ]]; then
   echo "Error: unknown OS type: \"$OSTYPE\""
   exit 1
