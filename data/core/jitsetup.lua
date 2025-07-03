00df4442ece55901e729ca2c1665be1119b0851e
jgmdev <jgmdev@gmail.com>
2025-07-03 00:35:49 -0400

    Use LuaJIT FFI to perform draw calls
    With this change we sort of bypass the Lua C API and instead call
    the rencache drawing functions directly. The overhead of calling
    into the drawing functions is reduced in around 90%, which is
    a significant amount given that these functions are basically
    the core rendering.

--- data/core/jitsetup.lua
+++ data/core/jitsetup.lua
@@ -70,3 +70,68 @@ else
   -- Somewhat arbitrary value (>= the default).
   jit.opt.start("sizemcode=128")
 end
+
+-- ffi overrides for faster rendering calls
+local ffi = require("ffi")
+
+ffi.cdef [[
+  void* ren_get_target_window(void);
+  void rencache_set_clip_rect_ffi(void *window_renderer, float x, float y, float w, float h);
+  void rencache_draw_rect_ffi(void *window_renderer, float x, float y, float w, float h, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+  double rencache_draw_text_ffi(void *window_renderer, void **font, const char *text, size_t len, double x, double y, unsigned char r, unsigned char g, unsigned char b, unsigned char a, double tab_offset);
+  void rencache_begin_frame_ffi(void *window_renderer);
+  void rencache_end_frame_ffi();
+]]
+
+renderer.draw_rect_lua = renderer.draw_rect
+function renderer.draw_rect(x, y, w, h, color, tab)
+  ffi.C.rencache_draw_rect_ffi(
+    ffi.C.ren_get_target_window(),
+    x, y, w, h,
+    color[1], color[2], color[3], color[4]
+  )
+end
+
+renderer.draw_text_lua = renderer.draw_text
+local fonts_native_cache = setmetatable({}, { __mode = "k" })
+function renderer.draw_text(font, text, x, y, color, tab)
+  if not fonts_native_cache[font] then
+    local fonts_list = font
+    if type(font) ~= "table" then
+      fonts_list = {font}
+    end
+    local fonts = ffi.new("void*[10]")
+    for i, f in pairs(fonts_list) do
+      fonts[i-1] = ffi.cast("void**", f)[0]
+    end
+    fonts[#fonts_list] = nil
+    fonts_native_cache[font] = fonts
+  end
+  local text = type(text) == "string" and text or tostring(text)
+  if not color then color = {255, 255, 255, 255} end
+  return ffi.C.rencache_draw_text_ffi(
+    ffi.C.ren_get_target_window(),
+    fonts_native_cache[font], text, #text, x, y,
+    color[1], color[2], color[3], color[4] or 255,
+    tab and tab.tab_offset or -1
+  );
+end
+
+renderer.begin_frame_lua = renderer.begin_frame
+local windows_native_cache = setmetatable({}, { __mode = "k" })
+function renderer.begin_frame(window)
+  if not windows_native_cache[window] then
+    windows_native_cache[window] = ffi.cast("void**", window)[0]
+  end
+  ffi.C.rencache_begin_frame_ffi(windows_native_cache[window])
+end
+
+renderer.end_frame_lua = renderer.end_frame
+function renderer.end_frame()
+  ffi.C.rencache_end_frame_ffi()
+end
+
+renderer.set_clip_rect_lua = renderer.set_clip_rect
+function renderer.set_clip_rect(x, y, w, h)
+  ffi.C.rencache_set_clip_rect_ffi(ffi.C.ren_get_target_window(), x, y, w, h)
+end
--- src/rencache.h
+++ src/rencache.h
@@ -13,4 +13,10 @@ void  rencache_invalidate(void);
 void  rencache_begin_frame(RenWindow *window_renderer);
 void  rencache_end_frame(RenWindow *window_renderer);
 
+/* Specialized equivalents for LuaJIT FFI use */
+EXPORT void rencache_set_clip_rect_ffi(RenWindow *window_renderer, float x, float y, float w, float h);
+EXPORT void rencache_draw_rect_ffi(RenWindow *window_renderer, float x, float y, float w, float h, unsigned char r, unsigned char g, unsigned char b, unsigned char a);
+EXPORT double rencache_draw_text_ffi(RenWindow *window_renderer, RenFont **font, const char *text, size_t len, double x, double y, unsigned char r, unsigned char g, unsigned char b, unsigned char a, double tab_offset);
+EXPORT void rencache_begin_frame_ffi(RenWindow *window_renderer);
+EXPORT void rencache_end_frame_ffi();
 #endif
