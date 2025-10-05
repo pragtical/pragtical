fb431d19ea99c5856787c5bbb610db8f69971aaa
Adam <adamdharrison@gmail.com>
2025-10-05 13:06:32 -0400

    Adjusted parameter order and docs for renwindow create. (#2155)

--- docs/api/renwindow.lua
+++ docs/api/renwindow.lua
@@ -8,15 +8,15 @@ renwindow = {}
 ---
 ---Create a new window
 ---
----@param str string? if nil will be undefined
+---@param title string the title given to the newly created window
 ---@param width integer? if nil or less than 1 will be calculated from display
 ---@param height integer? if nil or less than 1 will be calculated from display
 ---
 ---@return renwindow
-function renwindow.create(str, width, height) end
+function renwindow.create(title, width, height) end
 
 ---
---- Get width and height of a window
+---Get width and height of a window
 ---
 ---@param window renwindow
 ---
@@ -34,7 +34,7 @@ function renwindow.get_size(window) end
 function renwindow.get_refresh_rate(window) end
 
 ---
---- Restore Window
+---Restore Window
 ---
 ---@return number
 function renwindow._restore() end
