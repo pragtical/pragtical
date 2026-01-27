25ef9140280120224459898a0be86932f4b1af1a
jgmdev <jgmdev@gmail.com>
2026-01-27 14:54:19 -0400

    More consistent acceleration

--- data/core/view.lua
+++ data/core/view.lua
@@ -131,7 +131,7 @@ end
 local function move_constant(val, dest, duration, start_value, acceleration)
   local distance = math.abs(dest - start_value)
   local speed = distance / (core.fps * duration) -- px per second
-  speed = speed + ((acceleration * speed) - speed)
+  speed = acceleration * speed
   if val < dest then
     val = math.min(val + speed, dest)
   elseif val > dest then
@@ -183,12 +183,12 @@ function View:move_towards(t, k, dest, rate, name)
           1,                                   -- acceleration
           rate and lerp_duration(rate) or 0.15 -- rate
         }
-      elseif t[mk][3] ~= dest then
+      elseif math.abs(t[mk][3] - dest) >= 1 then
         t[mk][1] = val
         t[mk][3] = dest
         t[mk][4] = math.min(t[mk][4] + 0.2, 3)  -- increase acceleration
       else
-        t[mk][4] = math.max(t[mk][4] - 0.05, 1) -- decrease acceleration
+        t[mk][4] = math.max(t[mk][4] - 0.2, 1) -- decrease acceleration
       end
       rate = constant_scroll and 0.5 or t[mk][5]
       if config.animation_rate ~= 1 then
